5f55a43cd77e3d697bb90f9c4cf6fde2ce0d9b98
Fırat Tamur <ftamur16@ku.edu.tr>
2021-10-11 15:17:09 +0300

    chore: rename src to dea

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "dea"},
+    packages=setuptools.find_packages(where="dea"),
     python_requires=">=3.8",
 )
