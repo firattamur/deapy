5f9aeb56b4373b2959feca45dcf69c3825d3b763
Fırat Tamur <ftamur16@ku.edu.tr>
2021-10-11 15:09:13 +0300

    chore: change main directory name

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dea",
+    version="0.0.1",
+    author="firat tamur",
+    author_email="ftamur16@ku.edu.tr",
+    description="A Python Package for Data Envelopment Analysis.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/firattamur/dea-py",
+    project_urls={
+        "Bug Tracker": "https://github.com/firattamur/dea-py/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
